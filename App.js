29e7a0056a9e57fd50ced96d0a1369d17ec38a25
bungafdl <bungafdl374@gmail.com>
2018-09-05 19:51:43 +0700

    first commit

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Judul from'./Component/Judul';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Nama : Bunga Fadhillah</Text>
-        <Text>Kelas : XI RPL 3</Text>
-        <Text>Absen : 12</Text>
+export default class App extends React.Component{
+  render(){
+    return(
+      <View style={StyleSheet.container}>
+      <Judul/>
+      <Text>Nama : Bunga Fadhillah</Text>
+      <Text>Kelas : XI RPL 3</Text>
+      <Text>Absen : 12</Text>
+
+      <Image
+      style={{width:300, height: 300}}
+      source={require('./fotoku.jpg')}
+      />
       </View>
-    );
+      );
   }
 }
-
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems:'center',
     justifyContent: 'center',
   },
 });
---
+++ Component/Judul.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+class Judul extends React.Component{
+	render(){
+		return(
+			<Text style={salon.judul}
+			>BIODATA</Text>
+			)
+	}
+}
+const salon ={
+	judul:{
+		color : '#f00',
+		fontsize : 50,
+		backgroundColor: '#727272'
+	}
+}
+export default Judul;
